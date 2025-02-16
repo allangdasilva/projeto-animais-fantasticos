7debff9459182e28ea1b8e0f4357eaedb58a5e86
Allan Silva <allandasilva33@outlook.com>
2025-02-16 18:07:24 -0300

    Estilização do modal + interação com js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initTab from "./modules/tab.js";
 import initAccordion from "./modules/accordion.js";
 import initScrollSmooth from "./modules/scroll-smooth.js";
 import initiShowSections from "./modules/show-sections.js";
+import initiModal from "./modules/modal.js";
 
 initTab();
 initAccordion();
 initScrollSmooth();
 initiShowSections();
+initiModal();
