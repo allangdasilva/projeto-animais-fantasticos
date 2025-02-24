71a5e18add2658e5b3aa171b7fd44d27723e17d3
Allan Silva <allandasilva33@outlook.com>
2025-02-24 20:05:56 -0300

    Accordion transformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,17 +1,26 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll("[data-faq='accordion'] dt");
-  const ativoClass = "ativo";
-  if (accordionList.length) {
-    accordionList[0].classList.add(ativoClass);
-    accordionList[0].nextElementSibling.classList.add(ativoClass);
-
-    function activeAccordion(event) {
-      event.target.classList.toggle(ativoClass);
-      event.target.nextElementSibling.classList.toggle(ativoClass);
-    }
-
-    accordionList.forEach((ele) => {
-      ele.addEventListener("click", activeAccordion);
+export default class Accordion {
+  constructor(links) {
+    this.accordionList = document.querySelectorAll(links);
+    this.classActive = "ativo";
+    this.toggleAccordion = this.toggleAccordion.bind(this);
+  }
+  toggleAccordion(event) {
+    event.target.classList.toggle(this.classActive);
+    event.target.nextElementSibling.classList.toggle(this.classActive);
+  }
+  //adiciona eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((ele) => {
+      ele.addEventListener("click", this.toggleAccordion);
     });
+  }
+  //iniciar função
+  init() {
+    if (this.accordionList.length) {
+      this.addAccordionEvent();
+      this.accordionList[0].classList.add(this.classActive);
+      this.accordionList[0].nextElementSibling.classList.add(this.classActive);
+    }
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import initTab from "./modules/tab.js";
-import initAccordion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import ScrollSmooth from "./modules/scroll-smooth.js";
 import initiShowSections from "./modules/show-sections.js";
 import initiModal from "./modules/modal.js";
@@ -13,8 +13,10 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 const scrollSmooth = new ScrollSmooth('[data-menu="smooth"] a[href^="#"]');
 scrollSmooth.init();
 
+const accordion = new Accordion("[data-faq='accordion'] dt");
+accordion.init();
+
 initTab();
-initAccordion();
 initiShowSections();
 initiModal();
 initiTooltip();
